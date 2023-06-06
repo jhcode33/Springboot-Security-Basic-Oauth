ae6e1317a8a0342e29079ad781d1d34f7793e68d
jhcode33 <jhcode33@gmail.com>
2023-06-07 02:00:05 +0900

    Naver OAuth 로그인 성공

--- src/main/java/com/jhcode33/security/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/jhcode33/security/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.jhcode33.security.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.jhcode33.security.config.auth.PrincipalDetails;
 import com.jhcode33.security.config.oauth.provider.FacebookUserInfo;
 import com.jhcode33.security.config.oauth.provider.GoogleUserInfo;
+import com.jhcode33.security.config.oauth.provider.NaverUserInfo;
 import com.jhcode33.security.config.oauth.provider.OAuth2UserInfo;
 import com.jhcode33.security.model.User;
 import com.jhcode33.security.repository.UserRepository;
@@ -69,9 +72,14 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 			System.out.println("페이스북 로그인을 진행합니다.");
 			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
 			
+		} else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+			System.out.println("네이버 로그인을 진행합니다.");
+			oAuth2UserInfo = new NaverUserInfo((Map)oauth2User.getAttributes().get("response"));
+			
 		} else {
-			System.out.println("현재는 구글과 페이스북만 지원합니다.");
+			System.out.println("현재는 구글과 페이스북과 네이버만 지원합니다.");
 		}
+		
 		
 		String provider = oAuth2UserInfo.getProvider();
 		String providerId = oAuth2UserInfo.getProviderId();
@@ -83,6 +91,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		User userEntity = userRepository.findByUsername(username);
 		
 		if(userEntity == null) {
+			System.out.println("OAuth 자동 회원가입 진행합니다.");
 			userEntity = User.builder()
 					 .username(username)
 					 .password(password)
