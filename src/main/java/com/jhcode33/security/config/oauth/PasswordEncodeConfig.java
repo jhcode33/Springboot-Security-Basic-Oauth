d5921994b99f22795d90cbb7026f44c3cdd35394
jhcode33 <jhcode33@gmail.com>
2023-07-17 15:32:33 +0900

    passwordEncoder 순환참조 해결

---
+++ src/main/java/com/jhcode33/security/config/oauth/PasswordEncodeConfig.java
@@ -0,0 +1,16 @@
+package com.jhcode33.security.config.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncodeConfig {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
